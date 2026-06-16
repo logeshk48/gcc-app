90cfeefaf53cddc1904b5f621e27b7e1eb9e80e3
Logesh K <logeshlogesh093@gmail.com>
2026-06-16 22:38:10 +0530

    fix base path for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/gcc-app/',
-  resolve: {
-    alias: {
-      'react-is': 'react-is'
-    }
-  }
+  base: '/',
 })
